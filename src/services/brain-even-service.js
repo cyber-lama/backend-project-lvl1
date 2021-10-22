811f0ddd23f0bc2e3ae4a5a600aa4b27a8d2fa85
cyber-lama <dimmeln1k@yandex.ru>
2021-10-22 23:59:28 +0300

    ParityCheckGame v-2 make lint

--- src/services/brain-even-service.js
+++ src/services/brain-even-service.js
@@ -1,40 +1,40 @@
 import readlineSync from 'readline-sync';
-import greetUser from "../cli.js";
+import greetUser from '../cli.js';
 
 const generateRandNum = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-const checkEven = n => !(n % 2);
+const checkEven = (n) => !(n % 2);
 
-const showText = text => readlineSync.question(text);
+const showText = (text) => readlineSync.question(text);
 
+function playParityCheckGame() {
+  let count = 0;
+  let userName;
 
-function playParityCheckGame () {
-    let count = 0;
-    let userName;
+  const runGame = () => {
+    do {
+      if (count === 0) { // если это первый цикл
+        userName = greetUser(); // устанавливаем имя
+        showText('Answer "yes" if the number is even, otherwise answer "no".'); // правила игры
+      }
+      count += 1;
 
-    const runGame = () => {
-        do {
-            if(count === 0){ // если это первый цикл
-                userName = greetUser() // устанавливаем имя
-                showText('Answer "yes" if the number is even, otherwise answer "no".') // правила игры
-            }
-            count++;
+      const randNum = generateRandNum(1, 10);
+      const even = checkEven(randNum);
 
-            const randNum = generateRandNum(1, 10);
-            const even = checkEven(randNum);
-
-            showText(`Question: ${randNum}`)
-            const answer = readlineSync.question('How do you like it? ', {
-                trueValue: ['yes', 'y'],
-                falseValue: ['no', 'n']
-            });
-            if (answer !== even) {
-                return showText(`${answer} is wrong answer ;(. Correct answer was ${even}.`)
-            }
-            if(count === 3) return showText(`Congratulations, ${userName}!`);
-            showText("Correct!");
-        } while (count < 3);
-    }
-    runGame();
+      showText(`Question: ${randNum}`);
+      const answer = readlineSync.question('How do you like it? ', {
+        trueValue: ['yes', 'y'],
+        falseValue: ['no', 'n'],
+      });
+      if (answer !== even) {
+        return showText(`${answer} is wrong answer ;(. Correct answer was ${even}.`);
+      }
+      if (count === 3) return showText(`Congratulations, ${userName}!`);
+      showText('Correct!');
+    } while (count < 3);
+    return null;
+  };
+  runGame();
 }
 export default playParityCheckGame;
